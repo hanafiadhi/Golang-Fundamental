465c39b174dde4646cc0c7df58e73b3cd0ff6fc0
Hanafi Adhi Prasetyo <hanafi.4148@students.amikom.ac.id>
2022-12-27 10:02:16 +0700

    Belajar Golang Dasar

---
+++ String.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("This is A string")
+	fmt.Println(len("Hanafi Adhi Prasetyo"))
+	fmt.Println("ABCD"[0])
+}
---
+++ variabel.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Hanafi Adhi"
+	fmt.Println(name)
+
+	name = "Hanafi Adhi Prasetyo"
+	fmt.Println(name)
+
+	var kawan = "Gilbud"
+	fmt.Println(kawan)
+
+	kawan2 := "rudi"
+	fmt.Println(kawan2)
+	//multiple variabel
+	var (
+		firstname = "Hanafi"
+		lastname  = "adhi prasetyo"
+	)
+	fmt.Println(firstname, lastname)
+}
