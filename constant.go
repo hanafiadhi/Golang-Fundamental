f05ba2137633a8300b2176ed90410d097f6b6901
Hanafi Adhi Prasetyo <hanafi.4148@students.amikom.ac.id>
2022-12-27 10:44:26 +0700

    Belajar Golang Dasar

---
+++ constant.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	const firstname string = "hanafi"
+	const lastname = "adhi"
+	//
+	//multiple constant
+	// const (
+	// 	firstname string = "hanafi"
+	// 	lastname         = "adhi"
+	// )
+
+}
