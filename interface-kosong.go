b26bdf740079c320df4f241a4ce6fd0f990d1bf2
Hanafi Adhi Prasetyo <hanafi.4148@students.amikom.ac.id>
2022-12-31 13:27:14 +0700

    Belajar Golang Dasar

---
+++ interface-kosong.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func Ups(i int) interface{} {
+	if i == 1 {
+		return true
+	} else {
+		return "Ups"
+	}
+}
+
+func main() {
+	var data interface{} = Ups(3)
+	fmt.Println(data)
+}
